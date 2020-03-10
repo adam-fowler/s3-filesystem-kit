802a934609b9573dde1fd238e3334439b93d3c93
Adam Fowler <adamfowler71@gmail.com>
2020-03-10 15:16:26 +0000

    Added iOS,tvOS,watchOS platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "s3-filesystem-kit",
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v5)],
     products: [
         .library(name: "S3FileSystemKit", targets: ["S3FileSystemKit"]),
     ],
